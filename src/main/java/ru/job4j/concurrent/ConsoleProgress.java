e3645d0f11b055dcd27f2af79c9f6defb92c4a22
VladislavCat <kshift84@gmail.com>
2022-08-08 21:10:43 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            char[] chars = {'\\', '|', '/'};
+            int index = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                index = index == 3 ? 0 : index;
+                System.out.println("\r load" + chars[index]);
+                index++;
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Thread progress = new Thread(new ConsoleProgress());
+            progress.start();
+            Thread.sleep(1000);
+            progress.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
