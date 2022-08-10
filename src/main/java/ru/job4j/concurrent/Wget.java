1a2b0c8479b2c23b6f43c6dea010b9ff1610d2e7
VladislavCat <kshift84@gmail.com>
2022-08-10 20:20:10 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,19 +1,59 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int i = 0; i <= 100; i++) {
-                            System.out.print("\rLoading: " + i + "%");
-                            Thread.sleep(1000);
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-        );
-        thread.start();
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        char separatorURL = '/';
+        try (BufferedInputStream in
+                     = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream out =
+                     new FileOutputStream(url.substring(url.lastIndexOf(separatorURL) + 1))) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead = in.read(dataBuffer, 0, 1024);
+            while (bytesRead != -1) {
+                out.write(dataBuffer, 0, bytesRead);
+                Thread.sleep(1000);
+                bytesRead = in.read(dataBuffer, 0, 1024);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        validate(args);
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Указано неверное количество аргументов");
+        } else if (args[0].length() == 0 || (!args[0].contains("."))) {
+            throw new IllegalArgumentException("URL указан неверно");
+        }
+        try {
+            Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Указано не число");
+        }
     }
 }
