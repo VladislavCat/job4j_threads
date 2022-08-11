eec983a0f8c949194d3921af5f46f3c820314183
VladislavCat <kshift84@gmail.com>
2022-08-11 18:31:42 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -6,10 +6,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    public static final int ONESECONDINMSECOND = 1000;
 
     public Wget(String url, int speed) {
         this.url = url;
@@ -23,12 +25,22 @@ public class Wget implements Runnable {
                      = new BufferedInputStream(new URL(url).openStream());
              FileOutputStream out =
                      new FileOutputStream(url.substring(url.lastIndexOf(separatorURL) + 1))) {
-            byte[] dataBuffer = new byte[1024];
-            int bytesRead = in.read(dataBuffer, 0, 1024);
+            Date timeStart = new Date();
+            long secondIterate = 1;
+            byte[] dataBuffer = new byte[speed];
+            int downloadData = 0;
+            int bytesRead = in.read(dataBuffer, 0, speed);
             while (bytesRead != -1) {
+                downloadData += bytesRead;
+                if (downloadData >= speed) {
+                    if (new Date().getTime() < timeStart.getTime() + ONESECONDINMSECOND * secondIterate) {
+                        secondIterate++;
+                        downloadData = 0;
+                        Thread.sleep((timeStart.getTime() + ONESECONDINMSECOND * secondIterate - new Date().getTime()));
+                    }
+                }
                 out.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(1000);
-                bytesRead = in.read(dataBuffer, 0, 1024);
+                bytesRead = in.read(dataBuffer, 0, speed);
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -40,8 +52,10 @@ public class Wget implements Runnable {
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
+        Date startTime = new Date();
         wget.start();
         wget.join();
+        System.out.println((new Date().getTime() - startTime.getTime()) / 1000 + " секунд.");
     }
 
     public static void validate(String[] args) {
