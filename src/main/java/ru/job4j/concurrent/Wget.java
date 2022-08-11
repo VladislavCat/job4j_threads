4f53d1a0f982c0df8543391ae7b0b670ce5adc61
VladislavCat <kshift84@gmail.com>
2022-08-11 21:10:54 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -36,7 +36,8 @@ public class Wget implements Runnable {
                     if (new Date().getTime() < timeStart.getTime() + ONESECONDINMSECOND * secondIterate) {
                         secondIterate++;
                         downloadData = 0;
-                        Thread.sleep((timeStart.getTime() + ONESECONDINMSECOND * secondIterate - new Date().getTime()));
+                        Thread.sleep((ONESECONDINMSECOND - (timeStart.getTime() + ONESECONDINMSECOND * secondIterate
+                                - new Date().getTime()) / ONESECONDINMSECOND));
                     }
                 }
                 out.write(dataBuffer, 0, bytesRead);
@@ -54,6 +55,7 @@ public class Wget implements Runnable {
         Thread wget = new Thread(new Wget(url, speed));
         Date startTime = new Date();
         wget.start();
+        System.out.println((new Date().getTime() - startTime.getTime()) / 1000 + " секунд.");
         wget.join();
         System.out.println((new Date().getTime() - startTime.getTime()) / 1000 + " секунд.");
     }
