d3fb5c76d603c64f2a1a5ea62bb5b2abb4ef9379
VladislavCat <kshift84@gmail.com>
2022-08-26 20:54:14 +0300

    0. CAS - операции [#6859 #336332]

--- src/main/java/ru/job4j/concurrent/casoperation/CASCount.java
+++ src/main/java/ru/job4j/concurrent/casoperation/CASCount.java
@@ -3,16 +3,23 @@ package ru.job4j.concurrent.casoperation;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>();
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public void increment() {
         int tmpCount;
         do {
             tmpCount = count.get();
-        } while (count.compareAndSet(tmpCount, tmpCount + 1));
+        } while (!count.compareAndSet(tmpCount, tmpCount + 1));
     }
 
     public int get() {
         return count.get();
+    }
+
+    public static void main(String[] args) {
+        CASCount casCount = new CASCount();
+        casCount.increment();
+        casCount.increment();
+        System.out.println(casCount.get());
     }
 }
