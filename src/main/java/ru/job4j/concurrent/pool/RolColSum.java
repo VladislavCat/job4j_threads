492c7d9679b873c7fd6b183ba31890795931071d
VladislavCat <kshift84@gmail.com>
2022-09-05 16:17:32 +0300

    4. CompletableFuture [#361626]

--- src/main/java/ru/job4j/concurrent/pool/RolColSum.java
+++ src/main/java/ru/job4j/concurrent/pool/RolColSum.java
@@ -1,29 +1,14 @@
 package ru.job4j.concurrent.pool;
 
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class RolColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public Sums(int rowSum, int colSum) {
-            this.rowSum = rowSum;
-            this.colSum = colSum;
-        }
-    }
-
     public static Sums[] sum(int[][] matrix) {
         Sums[] sums = new Sums[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
-            int row = 0;
-            int col = 0;
-            for (int j = 0; j < matrix.length; j++) {
-                row += matrix[i][j];
-                col += matrix[j][i];
-            }
-            sums[i] = new Sums(row, col);
+            sums[i] = getSums(matrix, i);
         }
         return sums;
     }
@@ -38,14 +23,16 @@ public class RolColSum {
     }
 
     public static CompletableFuture<Sums> getTask(int[][] matrix, int defaultPos) {
-        return CompletableFuture.supplyAsync(() -> {
-            int row = 0;
-            int column = 0;
-            for (int i = 0; i < matrix.length; i++) {
-                row += matrix[defaultPos][i];
-                column += matrix[i][defaultPos];
-            }
-            return new Sums(row, column);
-        });
+        return CompletableFuture.supplyAsync(() -> getSums(matrix, defaultPos));
+    }
+
+    private static Sums getSums(int[][] matrix, int defaultPos) {
+        int row = 0;
+        int col = 0;
+        for (int j = 0; j < matrix.length; j++) {
+            row += matrix[defaultPos][j];
+            col += matrix[j][defaultPos];
+        }
+        return new Sums(row, col);
     }
 }
